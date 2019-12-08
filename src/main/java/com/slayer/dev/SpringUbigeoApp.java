b268a830efb8a8e1606f108de2f58a6e18e3f0a3
Slayer <bryan.antonio.alvarado.villalobos@gmail.com>
2019-12-08 00:28:51 -0500

    Creacion del paquete base del microservicio ubigeo base de datos h2

---
+++ src/main/java/com/slayer/dev/SpringUbigeoApp.java
@@ -0,0 +1,33 @@
+package com.slayer.dev;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringUbigeoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUbigeoApp.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+}
